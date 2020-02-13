17d0b89694ba0dc92bd97f75c94113450b2c65cc
AntoineT <antoinet@sicara.com>
2020-02-13 08:13:31 +0100

    Refactor test

--- src/game/test_game.py
+++ src/game/test_game.py
@@ -9,7 +9,7 @@ def game():
     return Game2048()
 
 
-def test_move_up(game):
+def test_grid_return_power_two(game):
     game._grid = np.array([
         [0, 0, 0, 2],
         [0, 2, 0, 0],
@@ -23,6 +23,15 @@ def test_move_up(game):
         [0, 0, 0, 0],
         [2, 2, 2, 0],
     ]))
+
+
+def test_move_up(game):
+    game._grid = np.array([
+        [0, 0, 0, 2],
+        [0, 2, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 0],
+    ]).astype(np.uint8)
 
     moved, score = game.move(Action2048.UP)
 
@@ -37,21 +46,13 @@ def test_move_up(game):
     assert moved == 4
 
 
-def test_move_left():
-    game = Game2048()
+def test_move_left(game):
     game._grid = np.array([
         [0, 0, 0, 2],
         [0, 2, 0, 0],
         [0, 0, 0, 0],
         [1, 1, 1, 0],
     ]).astype(np.uint8)
-
-    np.testing.assert_equal(game.grid, np.array([
-        [0, 0, 0, 4],
-        [0, 4, 0, 0],
-        [0, 0, 0, 0],
-        [2, 2, 2, 0],
-    ]))
 
     moved, score = game.move(Action2048.LEFT)
 
@@ -66,21 +67,13 @@ def test_move_left():
     assert moved == 4
 
 
-def test_move_right():
-    game = Game2048()
+def test_move_right(game):
     game._grid = np.array([
         [0, 0, 0, 2],
         [0, 2, 0, 0],
         [0, 0, 0, 0],
         [1, 1, 1, 1],
     ]).astype(np.uint8)
-
-    np.testing.assert_equal(game.grid, np.array([
-        [0, 0, 0, 4],
-        [0, 4, 0, 0],
-        [0, 0, 0, 0],
-        [2, 2, 2, 2],
-    ]))
 
     moved, score = game.move(Action2048.RIGHT)
 
@@ -95,21 +88,13 @@ def test_move_right():
     assert moved == 4
 
 
-def test_move_down():
-    game = Game2048()
+def test_move_down(game):
     game._grid = np.array([
         [0, 0, 1, 2],
         [0, 2, 1, 0],
         [0, 0, 2, 0],
         [1, 1, 2, 0],
     ]).astype(np.uint8)
-
-    np.testing.assert_equal(game.grid, np.array([
-        [0, 0, 2, 4],
-        [0, 4, 2, 0],
-        [0, 0, 4, 0],
-        [2, 2, 4, 0],
-    ]))
 
     moved, score = game.move(Action2048.DOWN)
 
@@ -120,26 +105,17 @@ def test_move_down():
         [2, 2, 8, 4],
     ]))
 
-
     assert score == 12
     assert moved == 5
 
 
-def test_game_over():
-    game = Game2048()
+def test_game_over(game):
     game._grid = np.array([
         [5, 3, 1, 1],
         [3, 4, 2, 3],
         [1, 6, 4, 1],
         [4, 1, 2, 4],
     ]).astype(np.uint8)
-
-    np.testing.assert_equal(game.grid, np.array([
-        [32, 8, 2, 2],
-        [8, 16, 4, 8],
-        [2, 64, 16, 2],
-        [16, 2, 4, 16],
-    ]))
 
     moved, score = game.play(Action2048.DOWN)
 
@@ -169,21 +145,13 @@ def test_game_over():
     assert game.game_over == True
 
 
-def test_is_game_over_when_tile_match_are_available():
-    game = Game2048()
+def test_is_game_over_when_tile_match_are_available(game):
     game._grid = np.array([
         [5, 2, 1, 1],
         [3, 4, 3, 5],
         [1, 6, 4, 1],
         [4, 1, 2, 4],
     ]).astype(np.uint8)
-
-    np.testing.assert_equal(game.grid, np.array([
-        [32, 4, 2, 2],
-        [8, 16, 8, 32],
-        [2, 64, 16, 2],
-        [16, 2, 4, 16],
-    ]))
 
     moved, score = game.play(Action2048.LEFT)
 
@@ -196,11 +164,10 @@ def test_is_game_over_when_tile_match_are_available():
     first_row = list(game.grid[0])
 
     assert first_row == [32, 4, 4, 2] or first_row == [32, 4, 4, 4]
-    assert game.game_over == False
+    assert not game.game_over
 
 
-def test_can_move_any_direction_when_cells_are_available():
-    game = Game2048()
+def test_can_move_any_direction_when_cells_are_available(game):
     game._grid = np.array([
         [5, 2, 1, 1],
         [3, 4, 0, 5],
@@ -214,8 +181,7 @@ def test_can_move_any_direction_when_cells_are_available():
     assert game.can_move(Action2048.LEFT)
 
 
-def test_can_move_when_no_cells_available():
-    game = Game2048()
+def test_can_move_when_no_cells_available(game):
     game._grid = np.array([
         [5, 2, 1, 1],
         [3, 4, 2, 5],
@@ -229,8 +195,7 @@ def test_can_move_when_no_cells_available():
     assert game.can_move(Action2048.LEFT)
 
 
-def test_can_move_special_case():
-    game = Game2048()
+def test_can_move_special_case(game):
     game._grid = np.array([
         [0, 0, 0, 0],
         [0, 0, 0, 0],
